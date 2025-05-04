38987d62d2ca7e98b734927965733243aa4e9c7b
Alejandro Castro <a1183297@uabc.edu.mx>
2025-05-03 21:38:16 -0700

    nada

---
+++ app/src/main/java/com/example/extrusor_interfaz_grafica/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.extrusor_interfaz_grafica
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.extrusor_interfaz_grafica.ui.theme.Extrusor_Interfaz_GraficaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Extrusor_Interfaz_GraficaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Extrusor_Interfaz_GraficaTheme {
+        Greeting("Android")
+    }
+}
